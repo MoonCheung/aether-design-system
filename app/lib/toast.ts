1d8eb2d44375ac85fe8e47eaab5cd7e5ba8231fa
MoonCheung <ikmoons.is@gmail.com>
2025-06-27 23:29:12 +0800

    chore: add library

---
+++ app/lib/toast.ts
@@ -0,0 +1,32 @@
+import queryString, { StringifyOptions } from 'query-string';
+
+type ToastType = 'success' | 'error' | 'loading' | 'blank' | 'custom';
+
+export const generateToastQuery = ({
+  message,
+  type,
+  url,
+  additionalQuery,
+  options,
+}: {
+  message: string;
+  type: ToastType;
+  url: string;
+  additionalQuery?: Record<string, unknown>;
+  options?: StringifyOptions;
+}) =>
+  queryString.stringifyUrl(
+    {
+      url,
+      query: {
+        toast_message: message,
+        toast_type: type,
+        ...additionalQuery,
+      },
+    },
+    {
+      arrayFormat: 'comma',
+      skipEmptyString: true,
+      ...options,
+    },
+  );
