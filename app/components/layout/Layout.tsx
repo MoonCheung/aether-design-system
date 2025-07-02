b92cd88adfcb8e8b1802c4405244e910388b5ae8
MoonCheung <ikmoons.is@gmail.com>
2025-07-02 23:10:04 +0800

    chore: add layout components

---
+++ app/components/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import type { Route } from './+types/home'
+import { Meta, Links, ScrollRestoration, Scripts } from "react-router";
+
+export default function Layout({ children }: {children: Route.ReactNode}) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
