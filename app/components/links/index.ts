b556731a399d731b065902cae9889c29a01fd24c
MoonCheung <ikmoons.is@gmail.com>
2025-08-11 21:05:59 +0800

    feat: primary link component

--- app/components/links/index.ts
+++ app/components/links/index.ts
@@ -1,3 +1,4 @@
 export { UnstyledLink, type UnstyledLinkProps } from './UnstyledLink.tsx'
 export { UnderlineLink } from './UnderlineLink.tsx'
 export { ButtonLink } from './ButtonLink.tsx'
+export { PrimaryLink } from './PrimaryLink.tsx'
