ccf049abfa0c1e3cbf38ed4f5d15977ed1f8b29d
MoonCheung <ikmoons.is@gmail.com>
2025-08-19 00:05:57 +0800

    feat: modal page

--- app/routes/sandbox/modal.page.tsx
+++ app/routes/sandbox/modal.page.tsx
@@ -1,8 +1,26 @@
 import type { Route } from "./+types/modal";
 import * as React from 'react';
+import { Button } from '~/components/buttons/index';
+import Layout from '~/components/layout/Layout';
 
-export default function Modal() {
+import { ExampleModal } from '~/routes/sandbox/components/index';
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: 'Modal' }]
+}
+
+export default function ModalPage() {
   return (
-    <div>hello 子路由: Modal</div>
+    <Layout>
+      <main>
+        <div className='layout min-h-screen py-20'>
+          <ExampleModal>
+            {({ openModal }) => (
+              <Button onClick={openModal}>Open Modal</Button>
+            )}
+          </ExampleModal>
+        </div>
+      </main>
+    </Layout>
   )
 }
